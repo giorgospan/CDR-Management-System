dde37d7f73058b6ab033a66906f924805ae5e0ec
giorgospan <giorgospan96@gmail.com>
2019-02-21 02:06:03 +0200

    Minor fixes on README

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ echo "========="
 echo
 
 TARGET="./build/werhauz"
-OPFILE="./input_files/input6.txt"
+OPFILE="./input_files/input3.txt"
 CONFILE="./input_files/config_file.txt"
 
-valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all $TARGET -h1 40 -h2 40 -s 1000000 -o $OPFILE
-# $TARGET -h1 10 -h2 10 -s 512 -o $OPFILE -c $CONFILE
+valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all $TARGET -h1 10 -h2 10 -s 1024 -o $OPFILE
+$TARGET -h1 10 -h2 10 -s 1024 -o $OPFILE -c $CONFILE
